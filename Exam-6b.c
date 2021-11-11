29b7b9d2e501c7d27db79c8b35690c9ba26bfa0f
JDzet <Jaroslpa@yandex.ru>
2021-11-11 19:41:42 +0000

    Exam6b

--- Exam-6b.c
+++ Exam-6b.c
@@ -8,6 +8,18 @@ int pressure[n];
 #define pmax 780
 #define pnorm 760
 
+int z = 0;
+void menu(int *pressure, int day)
+{
+   for(int i = 0; i < n; i++)
+   {
+      if(i == day-1)
+        { 
+            pressure[i] = -1;
+            z++;
+        }  
+    }
+}
 
 int main()
 {
@@ -23,36 +35,33 @@ int main()
     int exact = 0;
     int above = 0;
 
-    char a;
+    int choice = -1;
     int day;
 
-    int z = 0;
-    while(1)
+
+/////////////////////////////   
+    do
     {
-        printf("Удалить какие-либо результаты? (y/n): ");   
-        scanf("%c", &a);
+        printf("Удалить какие-либо результаты?\n");
+        printf("1 - да\n");
+        printf("2 - нет\n");
+        printf("choice: ");
+        scanf("%d", &choice);
 
-        if (a == 'y')
+       
+        switch(choice)
         {
-            printf("Введите дату: ");
-            scanf("%d", &day);
-
-            for(int i = 0; i < n; i++)
-            {
-              if(i == day-1) pressure[i] = -1;
-              z++;
-            }        }
-        if(a == 'n')
-        {
-            break;
+            case 1: printf("Введите дату: "); scanf("%d", &day); menu(pressure,day); break;
         }
+      
+        puts("\n");
     }
-    
-
+    while (choice != 2);
+///////////////////////////////////////////////////////////
 
     for(int i=0; i < n; i++)
     {
-        if(pressure[i] < pnorm) below ++;
+        if(pressure[i] < pnorm && pressure[i] != -1) below ++;
         if(pressure[i] == pnorm) exact ++;
         if(pressure[i] > pnorm) above ++;
     }
@@ -60,7 +69,7 @@ int main()
     printf("below: %d\n", below);
     printf("exact: %d\n", exact);
     printf("pmax: %d\n", above);
-    printf("%d\n",z);
+   // printf("%d\n",z);
     return 0;
 
 }
